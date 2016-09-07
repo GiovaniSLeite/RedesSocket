e8cd1d1eaf8d0445a6e6f93cce4548ae37d8f0e4
Giovani Sousa <sousa_giovani@hotmail.com>
2016-09-07 19:56:44 -0300

    Parte B - correção

--- WebServer.java
+++ WebServer.java
@@ -1,31 +1,23 @@
-import java.net.*;
-
+import java.net.ServerSocket;
+import java.net.Socket;
+ 
 public final class WebServer {
-
-    public static void main(String arvg[]) throws Exception {
-        // Ajustar o número da porta.
-        int port = 6789;
-
-        // Estabelecer o socket de escuta.
-        ServerSocket servidor = new ServerSocket(port);
-        System.out.println("Porta " + port + " aberta!");
-
-        // Processar a requisição de serviço HTTP em um laço infinito.
-        while (true) {
-            // Escutar requisição de conexão TCP.
-            Socket cliente = servidor.accept();
-
-            //Construir um objeto para processar a mensagem de requisição HTTP.
-            HttpRequest request = new HttpRequest(cliente);
-            
-            // Criar um novo thread para processar a requisição.
-            Thread thread = new Thread(request);
-            
-            //Iniciar o thread.
-            thread.start();
-
-        }
-    }
-
+ 
+    
+   public static void main(String argv[]) throws Exception
+ {
+     int port =6789;
+     ServerSocket server = new ServerSocket(port);
+     
+     while (true) {
+      // Listen for a TCP connection request.
+      Socket conexao = server.accept();
+      //Construct object to process HTTP request message
+      HttpRequest request = new HttpRequest(conexao);
+      
+      
+      Thread thread = new Thread(request);
+      thread.start(); //start thread
+     }
+ }
 }
-
