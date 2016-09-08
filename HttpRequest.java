1f79af62f610e8e83fd5622ad7922aeacb619102
Giovani Sousa <sousa_giovani@hotmail.com>
2016-09-08 00:20:09 -0300

    Retirei acentos dos comentarios
    Comentarios sem acentos para poder executar no terminal

--- HttpRequest.java
+++ HttpRequest.java
@@ -23,30 +23,29 @@ final class HttpRequest implements Runnable {
 
     private void processRequest() throws Exception {
 
-        // Obter uma referência para os trechos de entrada e saída do socket.
+        // Obter uma referencia para os trechos de entrada e saida do socket.
         InputStream is = socket.getInputStream();
         DataOutputStream os = new DataOutputStream(socket.getOutputStream());
 
         // Ajustar os filtros do trecho de entrada.
         BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
-        // Obter a linha de requisição da mensagem de requisição HTTP.
+        // Obter a linha de requisicao da mensagem de requisição HTTP.
         String requestLine = br.readLine();
 
-        //  Exibir a linha de requisição.
+        //  Exibir a linha de requisicao.
         System.out.println();
         System.out.println(requestLine);
 
-        // Extrair o nome do arquivo a linha de requisição.
+        // Extrair o nome do arquivo a linha de requisicao.
         StringTokenizer tokens = new StringTokenizer(requestLine);
-        //tokens.nextToken();
+
         tokens.nextToken(); // pular o método, que deve ser “GET”
         String fileName = tokens.nextToken();
-        // Acrescente um “.” de modo que a requisição do arquivo esteja dentro do diretório atual.
+        // Acrescente um “.” de modo que a requisicao do arquivo esteja dentro do diretorio atual.
         fileName = "." + fileName;
         System.out.println("Filename to Get: " + fileName);
-   
-        
+
         // Abrir o arquivo requisitado.
         FileInputStream fis = null;
         Boolean fileExists = true;
@@ -57,14 +56,14 @@ final class HttpRequest implements Runnable {
         }
 
         /*
-        Existem três partes para a mensagem de resposta: a linha de status, 
-        os cabeçalhos da resposta e o corpo da entidade. A linha de status e 
-        os cabeçalhos da resposta são terminados pela de seqüência de caracteres 
+        Existem tres partes para a mensagem de resposta: a linha de status, 
+        os cabecalhos da resposta e o corpo da entidade. A linha de status e 
+        os cabecalhos da resposta são terminados pela de sequencia de caracteres 
         CRLF. Iremos responder com uma linha de status, que armazenamos na 
-        variável statusLine, e um único cabeçalho de resposta, que armazenamos 
-        na variável contentTypeLine. No caso de uma requisição de um arquivo 
-        não existente, retornamos 404 Not Found na linha de status da mensagem 
-        de resposta e incluímos uma mensagem de erro no formato de um documento
+        variavel statusLine, e um unico cabeçalho de resposta, que armazenamos 
+        na variavel contentTypeLine. No caso de uma requisicao de um arquivo 
+        nao existente, retornamos 404 Not Found na linha de status da mensagem 
+        de resposta e incluimos uma mensagem de erro no formato de um documento
         HTML no corpo da entidade.
          */
         // Construir a mensagem de resposta.
@@ -79,14 +78,14 @@ final class HttpRequest implements Runnable {
             contentTypeLine = "Content-Type: text/html" + CRLF;
             entityBody = "<HTML>" + "<HEAD><TITLE>Nao encontrado</TITLE></HEAD>" + "<BODY>O arquivo requisitado nao foi encontrado.</BODY></HTML>";
         }
-        
+
         // Enviar a linha de status.
         os.writeBytes(statusLine);
 
-        // Enviar a linha de tipo de conteúdo.
+        // Enviar a linha de tipo de conteudo.
         os.writeBytes(contentTypeLine);
 
-        // Enviar uma linha em branco para indicar o fim das linhas de cabeçalho.
+        // Enviar uma linha em branco para indicar o fim das linhas de cabecalho.
         os.writeBytes(CRLF);
 
         // Enviar o corpo da entidade.
@@ -97,12 +96,12 @@ final class HttpRequest implements Runnable {
             os.writeBytes(entityBody);
         }
 
-        // Obter e exibir as linhas de cabeçalho.     
-   String headerLine = null;
-   while ((headerLine = br.readLine()).length() != 0) { 
-    System.out.println(headerLine);
-   }
-        
+        // Obter e exibir as linhas de cabecalho.     
+        String headerLine = null;
+        while ((headerLine = br.readLine()).length() != 0) {
+            System.out.println(headerLine);
+        }
+
         // Feche as cadeias e socket.
         os.close();
         br.close();
@@ -111,14 +110,14 @@ final class HttpRequest implements Runnable {
 
     private String contentType(String fileName) {
         if (fileName.endsWith(".htm") || fileName.endsWith(".html")) {
-                return "text/html";
-	}
+            return "text/html";
+        }
         //Implementar para outros tipos
-           if (fileName.endsWith(".gif")) {
-                return "image/gif"; 
+        if (fileName.endsWith(".gif")) {
+            return "image/gif";
         }
         if (fileName.endsWith(".jpeg") || fileName.endsWith(".jpg")) {
-                return "image/jpeg"; 
+            return "image/jpeg";
         }
         return "application/octet-stream";
     }
@@ -127,7 +126,7 @@ final class HttpRequest implements Runnable {
         // Construir um buffer de 1K para comportar os bytes no caminho para o socket.
         byte[] buffer = new byte[1024];
         int bytes = 0;
-        // Copiar o arquivo requisitado dentro da cadeia de saída do socket.
+        // Copiar o arquivo requisitado dentro da cadeia de saida do socket.
         while ((bytes = fis.read(buffer)) != -1) {
             os.write(buffer, 0, bytes);
         }
--- WebServer.java
+++ WebServer.java
@@ -1,27 +1,25 @@
+
 import java.net.ServerSocket;
 import java.net.Socket;
- 
+
 public final class WebServer {
- 
-    
-   public static void main(String argv[]) throws Exception
- {
-     // Ajustar o número da porta.
-     int port =6789;
-     
-     // Estabelecer o socket de escuta.
-     ServerSocket server = new ServerSocket(port);
-     System.out.println("Porta " + port + " aberta!");
-     // Processar a requisição de serviço HTTP em um laço infinito.
-     while (true) {
-      // Escutar requisição de conexão TCP.
-      Socket conexao = server.accept();
-      //Construir um objeto para processar a mensagem de requisição HTTP.
-      HttpRequest request = new HttpRequest(conexao);
-      
-      
-      Thread thread = new Thread(request); // Criar um novo thread para processar a requisição.
-      thread.start(); //Iniciar o thread.
-     }
- }
+
+    public static void main(String argv[]) throws Exception {
+        // Ajustar o numero da porta.
+        int port = 6789;
+
+        // Estabelecer o socket de escuta.
+        ServerSocket server = new ServerSocket(port);
+        System.out.println("Porta " + port + " aberta!");
+        // Processar a requisicao de serviço HTTP em um laço infinito.
+        while (true) {
+            // Escutar requisicao de conexao TCP.
+            Socket conexao = server.accept();
+            //Construir um objeto para processar a mensagem de requisicao HTTP.
+            HttpRequest request = new HttpRequest(conexao);
+
+            Thread thread = new Thread(request); // Criar um novo thread para processar a requisicao.
+            thread.start(); //Iniciar o thread.
+        }
+    }
 }
